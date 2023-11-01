0488ac41a642022b0e2550768ea6bd9a365da846
Glenn Ferrie <glenn@gfdata.io>
2023-11-01 11:45:22 -0400

    clean-up stocks api code file

--- stk.alphavantage/StocksAPI.cs
+++ stk.alphavantage/StocksAPI.cs
@@ -7,23 +7,20 @@ namespace stk.alphavantage
 {
     public static class StocksAPI
     {
-        static int api_pos = 0;
-        static string[] api_keys = new[] { "KDU2A8QDR8AD6KC5", "0VVZDI4MNL1Y3771", "3OK5PJT6LA9K2EX7" };
-        // TODO: replace the strings above with your keys, can use more than 1.
-
         [SqlFunction(DataAccess = DataAccessKind.Read, SystemDataAccess = SystemDataAccessKind.Read)]
         public static string GetQuote(string symbol)
         {
-            return APICall($"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={__NextAPIKey()}");
+            string fn = "GLOBAL_QUOTE";
+            return APICall($"{baseUrl}?function={fn}&symbol={symbol}&apikey={__NextAPIKey()}");
         }
 
         [SqlFunction(DataAccess = DataAccessKind.Read, SystemDataAccess = SystemDataAccessKind.Read)]
         public static string GetIntradaySeries(string symbol)
         {
             string interval = "5min";
-            return APICall($"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={__NextAPIKey()}");
+            string fn = "TIME_SERIES_INTRADAY";
+            return APICall($"{baseUrl}?function={fn}&symbol={symbol}&interval={interval}&apikey={__NextAPIKey()}");
         }
-
         private static string APICall(string url)
         {
             string output = string.Empty;
@@ -42,14 +39,18 @@ namespace stk.alphavantage
             return output;
         }
 
+        #region API Keys
         private static string __NextAPIKey()
         {
-            var next_apikey = api_keys[api_pos++];
-            if (api_pos == api_keys.Length)
-            {
-                api_pos = 0;
-            }
-            return next_apikey;
+            var nextKey = api_keys[api_pos++];
+            if (api_pos == api_keys.Length) { api_pos = 0; }
+            return nextKey;
         }
+        static int api_pos = 0;
+        static string[] api_keys = new[] { "KDU2A8QDR8AD6KC5", "0VVZDI4MNL1Y3771", "3OK5PJT6LA9K2EX7" };
+        // NOTE: replace the strings above with your keys, can use more than 1. or just use mine, whatever!
+        #endregion
+
+        const string baseUrl = "https://www.alphavantage.co/query";
     }
 }
